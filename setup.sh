bbba8ff464299d02c6f9cb30346ea40f14d29063
Jordan Murphy <jmurphy786@gmail.com>
2026-05-14 19:49:19 +0100

    test

--- setup.sh
+++ setup.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
 set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Use API to avoid cloning homebrew-core
-export HOMEBREW_INSTALL_FROM_API=1
-export HOMEBREW_NO_INSTALL_FROM_API=0
-
-# Install Homebrew if not already installed
-if [ ! -f /home/linuxbrew/.linuxbrew/bin/brew ]; then
-    echo "Installing Homebrew..."
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-echo "Installing Homebrew packages..."
+echo "?? Installing Homebrew packages..."
 PACKAGES=(
     stow
     zoxide
@@ -27,25 +16,23 @@ PACKAGES=(
     fzf
     lazygit
 )
-
 for package in "${PACKAGES[@]}"; do
     if brew list "$package" &>/dev/null; then
-        echo "$package already installed, skipping"
+        echo "V $package already installed, skipping"
     else
         echo "Installing $package..."
         brew install "$package"
     fi
 done
 
-echo "Stowing dotfiles..."
-rm -f ~/.bashrc
+rm -f ~/.bashrc 
+
+echo "?? Stowing dotfiles..."
 cd "$SCRIPT_DIR"
 stow --target="$HOME" */
 
-echo "Setting up tmux plugins..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 source ~/.bashrc
-tmux source-file ~/.tmux.conf
-
-echo "Done!"
+tmux source-file .tmux.conf
+echo "? Done!"
