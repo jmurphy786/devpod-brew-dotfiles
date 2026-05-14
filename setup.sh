58e784d3f5e8303b64d1c8e86c25d626b8166872
Jordan Murphy <jmurphy786@gmail.com>
2026-05-14 20:14:27 +0100

    fix

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Install Homebrew regardless
+echo "Installing Homebrew..."
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Setup Homebrew env
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-echo "?? Installing Homebrew packages..."
+echo "Installing Homebrew packages..."
 PACKAGES=(
     stow
     zoxide
@@ -16,23 +22,25 @@ PACKAGES=(
     fzf
     lazygit
 )
+
 for package in "${PACKAGES[@]}"; do
     if brew list "$package" &>/dev/null; then
-        echo "V $package already installed, skipping"
+        echo "$package already installed, skipping"
     else
         echo "Installing $package..."
         brew install "$package"
     fi
 done
 
-rm -f ~/.bashrc 
-
-echo "?? Stowing dotfiles..."
+echo "Stowing dotfiles..."
+rm -f ~/.bashrc
 cd "$SCRIPT_DIR"
 stow --target="$HOME" */
 
+echo "Setting up tmux plugins..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 source ~/.bashrc
-tmux source-file .tmux.conf
-echo "? Done!"
+tmux source-file ~/.tmux.conf
+
+echo "Done!"
